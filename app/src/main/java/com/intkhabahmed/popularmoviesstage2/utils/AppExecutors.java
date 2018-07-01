e9027a7fc76bf0a99f1f7e85aac468fb23bc8f88
Intkhab Ahmed <intkhab.ahmed64@gmail.com>
2018-07-01 23:50:31 +0530

    Implemented offline access to movies and making a movie favourite

--- app/src/main/java/com/intkhabahmed/popularmoviesstage2/utils/AppExecutors.java
+++ app/src/main/java/com/intkhabahmed/popularmoviesstage2/utils/AppExecutors.java
@@ -1,27 +1,21 @@
 package com.intkhabahmed.popularmoviesstage2.utils;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.support.annotation.NonNull;
-
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class AppExecutors {
     private Executor diskIO;
-    private Executor mainThreadIO;
     private static final Object LOCK = new Object();
     private static AppExecutors sInstance;
 
-    private AppExecutors(Executor diskIO, Executor mainThreadIO) {
+    private AppExecutors (Executor diskIO) {
         this.diskIO = diskIO;
-        this.mainThreadIO = mainThreadIO;
     }
 
     public static AppExecutors getInstance() {
-        if (sInstance == null) {
+        if(sInstance == null) {
             synchronized (LOCK) {
-                sInstance = new AppExecutors(Executors.newSingleThreadScheduledExecutor(), new MainThreadExecutor());
+                sInstance = new AppExecutors(Executors.newSingleThreadScheduledExecutor());
             }
         }
         return sInstance;
@@ -29,18 +23,5 @@ public class AppExecutors {
 
     public Executor diskIO() {
         return diskIO;
-    }
-
-    public Executor mainThreadIO() {
-        return mainThreadIO;
-    }
-
-    private static class MainThreadExecutor implements Executor {
-        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
-
-        @Override
-        public void execute(@NonNull Runnable command) {
-            mainThreadHandler.post(command);
-        }
     }
 }
